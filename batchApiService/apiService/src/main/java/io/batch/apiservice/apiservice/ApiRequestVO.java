a0562eab08811329d20d4dd462d0b3d0a321c119
JUNGSANGYUN <sdsd98987@gmail.com>
2023-06-01 18:20:01 +0900

    [배치] 실습

---
+++ batchApiService/apiService/src/main/java/io/batch/apiservice/apiservice/ApiRequestVO.java
@@ -0,0 +1,13 @@
+package io.batch.apiservice.apiservice;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiRequestVO {
+
+    private long id;
+    private ProductVO productVO;
+
+}
---
+++ batchApiService/apiService/src/main/java/io/batch/apiservice/apiservice/ProductVO.java
@@ -0,0 +1,18 @@
+package io.batch.apiservice.apiservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductVO {
+
+    private Long id;
+    private String name;
+    private int price;
+    private String type;
+}
